e3530498de5b83bb3b4246ef3aed31298293d996
sachinbidkikar <sachinbidkikar@gmail.com>
2023-04-08 11:44:37 +0530

    UC1 Hello world App

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hello World';
+
+  ngOnInit(): void {
+    this.title = "Hello From BL";
+  }
+}
